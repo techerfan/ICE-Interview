05c73e8b9c6b6e336c4ce359af4669fb4719d6e3
Erfan Derakhshani <techerfan@gmail.com>
2025-02-17 14:53:47 +0000

    Feat: implemented mysql db functionalities based on the calculator service requirements

---
+++ internal/db/mysql/cart.go
@@ -0,0 +1,37 @@
+package mysql
+
+import (
+	"context"
+	"errors"
+	"interview/internal/entity"
+
+	"gorm.io/gorm"
+)
+
+func (m *Mysql) CreateCart(ctx context.Context, cart entity.Cart) (entity.Cart, error) {
+	var model = mapEntityToCart(cart)
+	if err := m.db.WithContext(ctx).Create(&model).Error; err != nil {
+		return entity.Cart{}, err
+	}
+
+	return mapCartToEntity(model), nil
+}
+
+func (m *Mysql) UpdateCart(ctx context.Context, cart entity.Cart) error {
+	var model = mapEntityToCart(cart)
+
+	return m.db.WithContext(ctx).Save(&model).Error
+}
+
+func (m *Mysql) FindOpenCartBySessionID(ctx context.Context, sessionID string) (entity.Cart, bool, error) {
+	var cart Cart
+
+	if err := m.db.WithContext(ctx).Where("status = ? AND session_id = ?", entity.CartOpen).First(&cart).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return entity.Cart{}, false, nil
+		}
+		return entity.Cart{}, false, err
+	}
+
+	return mapCartToEntity(cart), true, nil
+}
---
+++ internal/db/mysql/cart_item.go
@@ -0,0 +1,63 @@
+package mysql
+
+import (
+	"context"
+	"errors"
+	"interview/internal/entity"
+
+	"gorm.io/gorm"
+)
+
+func (m *Mysql) CreateCartItem(ctx context.Context, cartItem entity.CartItem) (entity.CartItem, error) {
+	var model = mapEntityToCartItem(cartItem)
+	if err := m.db.WithContext(ctx).Create(&model).Error; err != nil {
+		return entity.CartItem{}, err
+	}
+
+	return mapCartItemToEntity(model), nil
+}
+
+func (m *Mysql) FindCartItemByID(ctx context.Context, id uint) (entity.CartItem, bool, error) {
+	var cartItem CartItem
+
+	if err := m.db.WithContext(ctx).Where("id = ?", id).First(&cartItem).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return entity.CartItem{}, false, nil
+		}
+		return entity.CartItem{}, false, err
+	}
+
+	return mapCartItemToEntity(cartItem), true, nil
+}
+
+func (m *Mysql) FindCartItemByProduct(ctx context.Context, cartID uint, product string) (entity.CartItem, bool, error) {
+	var cartItem CartItem
+
+	if err := m.db.WithContext(ctx).Where("cart_id = ? AND ProductName = ?", cartID, product).First(&cartItem).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return entity.CartItem{}, false, nil
+		}
+		return entity.CartItem{}, false, err
+	}
+
+	return mapCartItemToEntity(cartItem), true, nil
+}
+
+func (m *Mysql) FindCartItemsBycartID(ctx context.Context, cartID uint) ([]entity.CartItem, error) {
+	var cartItems []CartItem
+
+	if err := m.db.WithContext(ctx).Where("cart_id = ?", cartID).First(&cartItems).Error; err != nil {
+		return nil, err
+	}
+
+	items := make([]entity.CartItem, 0, len(cartItems))
+	for _, item := range cartItems {
+		items = append(items, mapCartItemToEntity(item))
+	}
+
+	return items, nil
+}
+
+func (m *Mysql) DeleteCartItemByID(ctx context.Context, id uint) error {
+	return m.db.WithContext(ctx).Where("id = ?", id).Delete(&CartItem{}).Error
+}
